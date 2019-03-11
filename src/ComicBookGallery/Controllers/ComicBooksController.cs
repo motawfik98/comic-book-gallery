89f9f13d86fd226ab03071786ec5a70dab313307
Mohamed Ahmed <motawfik1998@gmail.com>
2019-03-11 19:45:34 +0200

    Added comic book repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,21 +11,8 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() {Name = "Script", Role = "Dan Slott"},
-                    new Artist() {Name = "Pencils", Role = "Humberto Ramos"},
-                    new Artist() {Name = "Inks", Role = "Victor Olazaba"},
-                    new Artist() {Name = "Colors", Role = "Edgar Delgado"},
-                    new Artist() {Name = "Letters", Role = "Chris Eliopoulos"}
-                }
-            };
 
+            ComicBook comicBook = null;
             
 
             return View(comicBook);
